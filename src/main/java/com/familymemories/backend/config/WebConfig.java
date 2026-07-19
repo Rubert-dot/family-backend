09704bf8d97c58fe94ac56a063c5cb9eed083d8c
Rubert-dot <rubertrex2112@gmail.com>
2026-07-19 15:17:51 +0530

    fix: explicit high priority cors filter config

--- src/main/java/com/familymemories/backend/config/WebConfig.java
+++ src/main/java/com/familymemories/backend/config/WebConfig.java
@@ -1,12 +1,17 @@
 package com.familymemories.backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -17,18 +22,22 @@ public class WebConfig implements WebMvcConfigurer {
         Path uploadPath = Paths.get(userDir, "uploads").toAbsolutePath().normalize();
         String uploadFolderUrl = uploadPath.toUri().toString();
 
-       
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations(uploadFolderUrl)
                 .setCachePeriod(3600);
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
